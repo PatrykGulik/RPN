97c6cf40ea09ce30009ab3f28073ed6b20feab5b
gulik93 <11002010@uhi.ac.uk>
2026-02-15 12:54:12 +0000

    Basic ArrayStack class implements IStack interface

---
+++ bsc-sc-rpn/ArrayStack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bsc_sc_rpn
+{
+    public class ArrayStack<T> : IStack<T>
+    {
+        private T[] items;
+        private int currentIndex;
+
+        public ArrayStack(int capacity) 
+        {
+            items = new T[capacity];
+            currentIndex = 0;
+        }
+
+        public void Push(T item)
+        {
+            items[currentIndex++] = item;
+        }
+        public T Pop()
+        {
+            return items[currentIndex];
+        }
+
+        public T Peek()
+        {
+            return items[currentIndex];
+        }
+
+        public bool IsEmpty()
+        {
+            return items.Length == 0;
+        }
+    }
+}
